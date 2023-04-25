b433acae1fdbc3ad616abe3f6918ccf5b019081c
Sianernian <1916430401@qq.com>
2023-04-25 10:27:41 +0800

    client stream

--- client_stream/client_stream_client.go
+++ client_stream/client_stream_client.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"context"
+	pb "gRPC_protoc/client_stream/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"log"
+	"strconv"
+	"time"
+)
+
+var client pb.ClientStreamGoClient
+
+func main() {
+	conn, err := grpc.Dial(":8080", grpc.WithInsecure(), grpc.WithKeepaliveParams(keepalive.ClientParameters{
+		Time:                15 * time.Second,
+		Timeout:             2 * time.Second,
+		PermitWithoutStream: false,
+	}))
+	if err != nil {
+		log.Fatalf("grpc dial err:%v", err)
+	}
+	defer conn.Close()
+	client = pb.NewClientStreamGoClient(conn)
+	routeList()
+}
+
+func routeList() {
+	stream, err := client.RouteList(context.Background())
+	if err != nil {
+		log.Fatalf("client routeList err:%v", err)
+	}
+	for i := 0; i < 10; i++ {
+		err = stream.Send(&pb.ClientStreamRequest{
+			Data: "client stream grpc" + strconv.Itoa(i),
+		})
+		if err != nil {
+			log.Fatalf("stream.Send err:%v", err)
+		}
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("stream.CloseAndRecv() err:%v", err)
+	}
+	log.Println(res)
+}
--- client_stream/client_stream_service.go
+++ client_stream/client_stream_service.go
@@ -1,1 +1,54 @@
-package client_stream
+package main
+
+import (
+	pb "gRPC_protoc/client_stream/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+type ClientStreamService struct {
+	pb.UnimplementedClientStreamGoServer
+}
+
+func main() {
+	listenner, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("net.Listen err:%V", err)
+	}
+
+	keepAlivePar := keepalive.ServerParameters{
+		Time:    15 * time.Second,
+		Timeout: 2 * time.Second,
+	}
+	
+	grpcServer := grpc.NewServer(grpc.KeepaliveParams(keepAlivePar))
+	pb.RegisterClientStreamGoServer(grpcServer, &ClientStreamService{})
+	err = grpcServer.Serve(listenner)
+	if err != nil {
+
+		log.Fatalf("grpcServer serve err；%V", err)
+	}
+
+}
+
+func (c *ClientStreamService) RouteList(srv pb.ClientStreamGo_RouteListServer) error {
+	for {
+		// 从流中 获取信息
+		res, err := srv.Recv()
+		if err == io.EOF {
+			return srv.SendAndClose(&pb.ClientStreamResponse{
+				Code:  int32(1),
+				Value: "ok",
+			})
+		}
+		if err != nil {
+			log.Fatalf("srv.Recv() err:%v", err)
+		}
+		log.Println(res.Data)
+	}
+	return nil
+}
